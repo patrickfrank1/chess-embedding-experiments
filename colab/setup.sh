0f851e8e57624e81a74258ff209ec2a9d3ce4df7
Patrick Frank <patr.frank@gmx.de>
2023-09-18 00:55:09 +0200

    Update colab notebook

--- colab/setup.sh
+++ colab/setup.sh
@@ -1,4 +1,27 @@
-PROJECT_DIR="/content/chess-embedding-experiments"
-cd $PROJECT_DIR
-python -m pip install -r requirements.txt --ignore-installed
-dvc pull
+#!/bin/bash
+
+cd /content/chess-embedding-experiments/colab
+
+# Load environment variables from .env file
+if [ -f .env ]; then
+    source .env
+else
+    echo "Error: .env file not found."
+    exit 1
+fi
+
+# install dependencies
+python -m pip install -r /content/chess-embedding-experiments/requirements.txt --ignore-installed
+
+# set up git
+git config --global user.email "$GITHUB_EMAIL"
+git config --global user.name "$GITHUB_SIGNATURE_NAME"
+echo "https://$GITHUB_USER_NAME:$GITHUB_TOKEN@github.com/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME.git"
+git remote add origin "https://$GITHUB_USER_NAME:$GITHUB_TOKEN@github.com/$GITHUB_REPO_OWNER/$GITHUB_REPO_NAME.git"
+
+# set up dvc
+dvc remote add colab_origin s3://dvc
+dvc remote modify colab_origin endpointurl "https://dagshub.com/$DAGSHUB_REPO_OWNER/$DAGSHUB_REPO_NAME.s3"
+dvc remote modify colab_origin --local access_key_id "$DAGSHUB_TOKEN"
+dvc remote modify colab_origin --local secret_access_key "$DAGSHUB_TOKEN"
+dvc pull -r colab_origin
