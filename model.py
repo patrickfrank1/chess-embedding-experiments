75113a658ce6e196f465a20eabbea4fa43ef11a7
Patrick Frank <patr.frank@gmx.de>
2023-09-23 12:32:28 +0200

    Add new data

--- model.py
+++ model.py
@@ -114,7 +114,7 @@ def trivial() -> dict[str, keras.Model]:
 	return {'autoencoder': autoencoder}
 
 def trivial2() -> dict[str, keras.Model]:
-	EMBEDDING_SIZE = 512
+	EMBEDDING_SIZE = 256
 	dtype = tf.bfloat16
 
 	# Encoder
