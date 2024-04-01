6776ba76b762f18bdec8f215bcdc60fdbd391436
Patrick Frank <patr.frank@gmx.de>
2024-04-01 13:44:13 +0200

    Refactor representation converter functions

--- src/preprocessing/board_representation.py
+++ src/preprocessing/board_representation.py
@@ -184,3 +184,17 @@ def tensor_to_board(tensor: np.ndarray, threshold: float = 0.5) -> chess.Board:
     # set turn
     reconstructed_board.turn = tensor[0, 0, 14]
     return reconstructed_board
+
+
+def boolean_to_byte_vector(boolean_vector: np.ndarray) -> bytes:
+    assert boolean_vector.dtype in [bool, int]
+    uint8_packed_vector = np.packbits(boolean_vector, axis=-1)
+    binary_vector = [bytes(vector.tolist()) for vector in uint8_packed_vector]
+    return binary_vector
+
+
+def byte_to_boolean_vector(byte_vector: list[bytes], original_shape: Tuple[int, int]):
+    unpacked_array = np.frombuffer(byte_vector, dtype=np.uint8)
+    unpacked_bits = np.unpackbits(unpacked_array)
+    unpacked_bits = unpacked_bits[:np.prod(original_shape)].reshape(original_shape)
+    return unpacked_bits
--- test/test_vector_db.py
+++ test/test_vector_db.py
@@ -1,8 +1,8 @@
-from typing import Generator, Tuple
 import pytest
 import numpy as np
 
 from src.search.db import MilvusVectorStore
+from src.preprocessing.board_representation import boolean_to_byte_vector, byte_to_boolean_vector
 
 
 def generate_embeddings(num_embeddings, dimensions, embedding_type):
@@ -15,18 +15,7 @@ def generate_embeddings(num_embeddings, dimensions, embedding_type):
         return raw_vector, binary_vector
 
 
-def boolean_to_byte_vector(boolean_vector: np.ndarray) -> bytes:
-    assert boolean_vector.dtype in [bool, int]
-    uint8_packed_vector = np.packbits(boolean_vector, axis=-1)
-    binary_vector = [bytes(vector.tolist()) for vector in uint8_packed_vector]
-    return binary_vector
 
-
-def byte_to_boolean_vector(byte_vector: list[bytes], original_shape: Tuple[int, int]):
-    unpacked_array = np.frombuffer(byte_vector, dtype=np.uint8)
-    unpacked_bits = np.unpackbits(unpacked_array)
-    unpacked_bits = unpacked_bits[:np.prod(original_shape)].reshape(original_shape)
-    return unpacked_bits
 
 
 @pytest.fixture(params=["float", "binary"])
